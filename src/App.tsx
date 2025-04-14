c072200f68a5d017bafd712432caa00f34e98216
Igor Paslauski Pedroso de Oliveira <io006235@cfjl.com.br>
2025-04-14 14:58:29 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/portifolioIgor" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
